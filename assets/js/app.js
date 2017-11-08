fbb0e04966cbdf728abe47f0b153a56e73f8224c
Paula Andrea Giraldo <andreapaulagm@gmail.com>
2017-11-08 03:03:10 -0300

    Primera Versión

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+/*Se debe alternar entre ocultar panel de añadir lista y agregar  
+ formulario*/
+
+/*Se llaman los elementos*/
+
+  var boton = document.getElementByID("bton");
+  boton.addEventListener("click", function ()){
+
+
+  var tareaUno = document.getElementsByClass("container").value;
+
+
+  var tareaDos = document.getElementsByIClass("tarea").value;
+
+  /*alternar paneles
+  if()
+
+
+
+ }
