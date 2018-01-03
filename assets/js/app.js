38dfc82092ac53ef316494eb153dab28e8c2d9d5
Paula Andrea Giraldo <andreapaulagm@gmail.com>
2018-01-02 22:28:52 -0300

    tercera versión

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,50 @@
-//crear evento boton
-var tecla= document.getElementById("boton");
-tecla.addEventListener("click", function (){
+	//crear evento boton
+	/*var tecla= document.getElementById("boton");
+	tecla.addEventListener("click", function (){
 
-//Limpiar area de tarea 
- var tarea = document.getElementById("tarea").value;
- document.getElementById("tarea").value = ""; 
+	//Limpiar area de tarea
+	 var tarea = document.getElementById("tarea").value;
+	 document.getElementById("tarea").value = "";
 
- //Lista de tareas
- var contenedor = document.getElementById("contenedor");
+	 //Lista de tareas
+	 var contenedor = document.getElementById("contenedor");
 
-var newTarea = document.createElement("div");
-
-//Oculta Formulario 
-function ocultar(){
-document.getElementsByclass('formulario').style.display = 'none';}
-
-  
-
-  
+	var newTarea = document.createElement("div");*/
 
 
- 
-  
-  
-  
+
+	/*Cuadro inicial para añadir texto
+	function lista_click(){
+		var lista = document.getElementsByClassName("lista").value;
+		lista.addEventListener("click")
+	}*/
+
+	/*Cear Lista de tareas*/
+
+		function listaTareas(){
+
+			var tarea = document.getElementById("tareaInput").value;
+			var divListaTareas = document.getElementById('listaTareas')
+			var divTarea = document.createElement ("div");
+			divTarea.innerHTML = (tarea);
+			divListaTareas.appendChild(divTarea);
+	}
 
 
-})
+	/*/mostrar y ocultar lista de tareas
+	function ocultar_mostrar(){
+		document.getElementsByclass('tareaInput').style.display = 'none';
+		document.getElementsByclass('tareaInput').style.display = 'visibility';
+	}
+
+	//Ocultar lista tareas
+	function mostar_ocultar(){
+		document.getElementById('tareaInput').value = "";
+			document.getElementsByClassName("class").style.display = "none";
+		var tarea= document.getElementsByClassName(".tarea");
+		if(tarea.style.display == "none"){
+			mostrar();
+		}else{
+			ocultar();
+		}
+	}*/
